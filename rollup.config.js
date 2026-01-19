1f5f14c0132f7e98e35d8ce50950bb47001219f0
ijunfu <ijunfu@163.com>
2026-01-19 21:40:06 +0800

    Add UMD

--- rollup.config.js
+++ rollup.config.js
@@ -11,6 +11,12 @@ export default {
     {
       file: 'dist/index.esm.js',
       format: 'esm',
+    },
+    {
+      file: 'dist/index.umd.js',
+      format: 'umd',
+      name: 'setRem', // 全局变量名，浏览器中用 window.setRem
+      exports: 'default'
     }
   ],
   plugins: [resolve(), commonjs()]
